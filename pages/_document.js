a4fbdcb7c92ebacea18ee9777d3290719a2a44e9
luc <luc.leray@gmail.com>
2018-02-05 00:30:39 +0800

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,8 @@ export default class MyDocument extends Document {
           <title>lucleray.me</title>
           <meta name='viewport' content='width=device-width, initial-scale=1.0' key='viewport' />
           <meta name='description' content='Software engineer with a passion for machine learning' key='description' />
+          <link rel='icon' type='image/png' sizes='32x32' href='static/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='static/favicon-16x16.png' />
           {this.props.styleTags}
         </Head>
         <body>
