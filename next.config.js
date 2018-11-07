a961186d26cd954831780c1d25dce82abbc97f03
luc <luc.leray@gmail.com>
2018-11-07 17:25:03 +0800

    only create browser stats in build-stats

--- next.config.js
+++ next.config.js
@@ -1,25 +1,15 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
-const bundleAnalyzerConfigExtra = {
-  analyzerMode: 'static',
-  openAnalyzer: false
-}
-
-const bundleAnalyzerConfig = {
-  browser: {
-    reportFilename: `../build-stats/webpack-browser.html`,
-    ...bundleAnalyzerConfigExtra
-  },
-  server: {
-    reportFilename: `../../build-stats/webpack-server.html`,
-    ...bundleAnalyzerConfigExtra
-  }
-}
-
 module.exports = phase =>
   withBundleAnalyzer({
     analyzeBrowser: phase === PHASE_PRODUCTION_BUILD,
     analyzeServer: phase === PHASE_PRODUCTION_BUILD,
-    bundleAnalyzerConfig
+    bundleAnalyzerConfig: {
+      browser: {
+        reportFilename: `../build-stats/webpack-browser.html`,
+        analyzerMode: 'static',
+        openAnalyzer: false
+      }
+    }
   })
