8aa2ca49d72e7dfba65ca88d6aa23c3a0ee9a3b9
luc <luc.leray@gmail.com>
2018-11-10 10:57:29 +0800

    Revert "attempt to fix NO_STATUS_CODE_FROM_LAMBDA"
    This reverts commit 84a475b89d0fc963fc463051a9254160e0f72695.

--- analytics/index.js
+++ analytics/index.js
@@ -33,7 +33,7 @@ module.exports = async (req, res) => {
       }
       nbViews[page]++
 
-      send(res, 200, {})
+      send(res, 200)
       return
     }
 
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
       return
     }
 
-    throw createError(404, 'page not found')
+    send(res, 404)
   } catch (_error) {
     const error = _error._apiError ? _error : createError()
     send(res, error.status, { error: error.message })
