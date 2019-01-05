ae8f64ab3a38697f15bd7023fd70fe5e458e4d8c
Luc <luc.leray@gmail.com>
2019-01-05 17:42:53 +0800

    Update packages (#6)
    * update next to canary
    
    * update react/react-dom
    
    * update to react@alpha
    
    * update .nowignore
    
    * add .prettierrc
    
    * remove next-size (part of nextjs now)
    
    * remove next-size (bis)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-const withSize = require('next-size')
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
 module.exports = phase =>
-  withSize(
-    withBundleAnalyzer({
-      analyzeBrowser: phase === PHASE_PRODUCTION_BUILD,
-      bundleAnalyzerConfig: {
-        browser: {
-          reportFilename: `../build-stats/webpack-browser.html`,
-          analyzerMode: 'static',
-          openAnalyzer: false
-        }
+  withBundleAnalyzer({
+    analyzeBrowser: phase === PHASE_PRODUCTION_BUILD,
+    bundleAnalyzerConfig: {
+      browser: {
+        reportFilename: `../build-stats/webpack-browser.html`,
+        analyzerMode: 'static',
+        openAnalyzer: false
       }
-    })
-  )
+    }
+  })
