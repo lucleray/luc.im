37daa780c422d9c46a6fc90e33817ab1edff3b51
luc <luc.leray@gmail.com>
2019-01-05 18:17:14 +0800

    add analyzeServer

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 module.exports = phase =>
   withBundleAnalyzer({
     analyzeBrowser: phase === PHASE_PRODUCTION_BUILD,
+    analyzeServer: phase === PHASE_PRODUCTION_BUILD,
     bundleAnalyzerConfig: {
       browser: {
         reportFilename: `../build-stats/webpack-browser.html`,
