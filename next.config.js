4cadfe491f74ca088e7f03ac8e54159e656918df
luc <luc.leray@gmail.com>
2019-01-05 17:40:52 +0800

    remove next-size (part of nextjs now)

--- next.config.js
+++ next.config.js
@@ -3,15 +3,13 @@ const withSize = require('next-size')
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
 module.exports = phase =>
-  withSize(
-    withBundleAnalyzer({
-      analyzeBrowser: phase === PHASE_PRODUCTION_BUILD,
-      bundleAnalyzerConfig: {
-        browser: {
-          reportFilename: `../build-stats/webpack-browser.html`,
-          analyzerMode: 'static',
-          openAnalyzer: false
-        }
+  withBundleAnalyzer({
+    analyzeBrowser: phase === PHASE_PRODUCTION_BUILD,
+    bundleAnalyzerConfig: {
+      browser: {
+        reportFilename: `../build-stats/webpack-browser.html`,
+        analyzerMode: 'static',
+        openAnalyzer: false
       }
-    })
-  )
+    }
+  })
