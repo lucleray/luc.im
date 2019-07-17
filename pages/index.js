1d33c749e5a519bf0b766ad97f25bd23bf0e8a86
Luc <luc.leray@gmail.com>
2019-07-17 02:40:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import links from '../lib/links'
 
 const Presentation = () => (
   <section aria-labelledby="presentation-title">
-    <H1 id="presentation-title">hi, i'm luc</H1>
+    <H1 id="presentation-title">hi, i'm luc aaaa</H1>
     <Card>I'm a french software engineer working in Paris, France.</Card>
     <Card>
       I am cofounder and CTO at{' '}
