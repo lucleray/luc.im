d89c11ca8015b19dcf3c9032293ca85f06c3d193
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-07-31 20:13:52 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Presentation = () => (
       I'm a french software engineer working at{' '}
       <A href="https://zeit.co">ZEIT</A>, building a for developers and
       designers to deploy their applications to the cloud gfdgdf gfdgdf xxxxxx.
-  <img src="https://source.unsplash.com/random/2000x2000" />
+  <img src="https://source.unsplash.com/random/800x600" />
     </Card>
     <Card>
       I started learning about websites{' '}
