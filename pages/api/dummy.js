d2e7797435d987d7a25fc9ed9b28b8760ab02483
luc <luc.leray@gmail.com>
2020-02-22 19:10:35 +0100

    improve dummy endpoint

--- pages/api/dummy.js
+++ pages/api/dummy.js
@@ -3,5 +3,16 @@ export default (req, res) => {
   const headers = req.headers
   const body = req.body
 
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, PUT, POST, DELETE, OPTIONS'
+  )
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Authorization, Accept, Content-Type'
+  )
+
   res.json({ method, headers, body })
 }
