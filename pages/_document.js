e35814bdc513c1eef50a5ff0df3b4ee799306ba0
luc <luc.leray@gmail.com>
2020-12-13 02:28:31 +0100

    remove favicon and og

--- pages/_document.js
+++ pages/_document.js
@@ -3,19 +3,10 @@ import { ServerStyleSheet } from 'styled-components/macro'
 
 const darkModeCode = `(function() {
 
-  function changeFavicon(v) {
-    var src = 'assets/favicon-' + (v ? 'dark' : 'light');
-    try {
-      document.getElementById('favicon-32').href = src + '-32x32.png';
-      document.getElementById('favicon-16').href = src + '-16x16.png';
-    } catch (err) {}
-  }
-
   function setDarkMode(v) {
     window.__darkMode = v
     localStorage.setItem('dark', v ? 'yes' : 'no');
     document.body.className = v ? 'dark' : 'light';
-    changeFavicon(v);
   }
 
   var q = window.matchMedia('(prefers-color-scheme: dark)');
@@ -62,28 +53,6 @@ export default class MyDocument extends Document {
             name="description"
             content="Software engineer with a passion for machine learning"
             key="description"
-          />
-          <link
-            id="favicon-32"
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="assets/favicon-light-32x32.png"
-          />
-          <link
-            id="favicon-16"
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="assets/favicon-light-16x16.png"
-          />
-          <meta
-            property="og:image"
-            content="https://lucleray.me/assets/cover.png"
-          />
-          <meta
-            property="og:image:secure_url"
-            content="https://lucleray.me/assets/cover.png"
           />
           {this.props.styleTags}
           {process.env.NODE_ENV === 'production' && (
