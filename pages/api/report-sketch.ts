a2e847674bfa80ce6a90883671ef5f08cd2aa55d
luc <luc.leray@gmail.com>
2021-01-05 11:35:19 +0100

    log b64url state

--- pages/api/report-sketch.ts
+++ pages/api/report-sketch.ts
@@ -1,7 +1,11 @@
 import { NextApiHandler } from 'next'
+import { encodeBase64URL } from '../../lib/b64url'
 
 const reportSketch: NextApiHandler = (req, res) => {
-  console.log(req.body)
+  console.log(
+    'http://localhost:3000?sketch=' + encodeBase64URL(JSON.stringify(req.body))
+  )
+
   return res.send('ok')
 }
 
