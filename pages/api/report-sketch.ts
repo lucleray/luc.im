4df6057b695cc9d5cff713418f9f12cc0bad1b4f
luc <luc.leray@gmail.com>
2021-01-05 17:02:44 +0100

    report sketch to airtable

--- pages/api/report-sketch.ts
+++ pages/api/report-sketch.ts
@@ -1,12 +1,35 @@
 import { NextApiHandler } from 'next'
 import { encodeBase64URL } from '../../lib/b64url'
 
-const reportSketch: NextApiHandler = (req, res) => {
+const reportSketch: NextApiHandler = async (req, res) => {
   const b64sketch = encodeBase64URL(JSON.stringify(req.body))
   const origin = req.headers.origin ?? 'https://luc.im'
   const previewSketchURL = `${origin}/?sketch=${b64sketch}`
 
-  console.log(previewSketchURL)
+  const resAirtable = await fetch(
+    'https://api.airtable.com/v0/appL5KS2qu2qvVyuA/Data',
+    {
+      method: 'POST',
+      headers: {
+        authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        records: [
+          { fields: { Timestamp: new Date(), Preview: previewSketchURL } }
+        ]
+      })
+    }
+  )
+  const jsonAirtable = await resAirtable.json()
+
+  if (!resAirtable.ok) {
+    throw new Error(
+      `Failed to upload sketch to Airtable, got ${res.status}: ${JSON.stringify(
+        jsonAirtable
+      )}`
+    )
+  }
 
   return res.send(null)
 }
