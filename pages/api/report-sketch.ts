72636aab2ffce8356c362595ca852b589c9d9bac
luc <luc.leray@gmail.com>
2021-01-05 11:40:28 +0100

    consider origin

--- pages/api/report-sketch.ts
+++ pages/api/report-sketch.ts
@@ -2,9 +2,11 @@ import { NextApiHandler } from 'next'
 import { encodeBase64URL } from '../../lib/b64url'
 
 const reportSketch: NextApiHandler = (req, res) => {
-  console.log(
-    'http://localhost:3000?sketch=' + encodeBase64URL(JSON.stringify(req.body))
-  )
+  const b64sketch = encodeBase64URL(JSON.stringify(req.body))
+  const origin = req.headers.Origin ?? 'https://luc.im'
+  const previewSketchURL = `${origin}/?sketch=${b64sketch}`
+
+  console.log(previewSketchURL)
   return res.send(null)
 }
 
