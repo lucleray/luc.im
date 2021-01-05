e0f6ed8b7f30278c6d9876bf52d5f5e199fd01cc
luc <luc.leray@gmail.com>
2021-01-05 12:43:56 +0100

    fix origin

--- pages/api/report-sketch.ts
+++ pages/api/report-sketch.ts
@@ -3,7 +3,7 @@ import { encodeBase64URL } from '../../lib/b64url'
 
 const reportSketch: NextApiHandler = (req, res) => {
   const b64sketch = encodeBase64URL(JSON.stringify(req.body))
-  const origin = req.headers.Origin ?? 'https://luc.im'
+  const origin = req.headers.origin ?? 'https://luc.im'
   const previewSketchURL = `${origin}/?sketch=${b64sketch}`
 
   console.log(previewSketchURL)
