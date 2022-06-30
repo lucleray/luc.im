7dbd95b00fe8279de2d721bc8c694aec690e636b
luc <luc.leray@gmail.com>
2022-07-01 00:27:48 +0200

    migrate _app and _document to ts

--- pages/_app.js
+++
@@ -1,4 +0,0 @@
-import '../components/system2/style.css'
-import '../components/system2/photos.css'
-
-export default ({ Component, pageProps }) => <Component {...pageProps} />
---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../components/system2/style.css'
+import '../components/system2/photos.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
