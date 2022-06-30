93e3956567c31b0f53644a1dc73dc15ae0f480b6
Luc Leray <luc.leray@gmail.com>
2022-07-01 00:25:30 +0200

    Remove styled components and upgrade Next (#64)
    * migrate to static css
    
    * remove styled-components deps
    
    * remove macros
    
    * update yarn.lock
    
    * upgrade next
    
    * fix _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components/macro'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 const darkModeCode = `(function() {
 
@@ -23,49 +22,37 @@ const darkModeCode = `(function() {
 
 })();`
 
-export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    )
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
-  }
-
-  render() {
-    return (
-      <html lang="en">
-        <Head>
-          <link
-            rel="preload"
-            href="/assets/font/inter-var-latin.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-            key="viewport"
-          />
-          <meta
-            name="description"
-            content="Software engineer with a passion for machine learning"
-            key="description"
-          />
-          {this.props.styleTags}
-        </Head>
-        <body>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: darkModeCode
-            }}
-          />
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/assets/font/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+          key="viewport"
+        />
+        <meta
+          name="description"
+          content="Software engineer with a passion for machine learning"
+          key="description"
+        />
+      </Head>
+      <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: darkModeCode
+          }}
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
