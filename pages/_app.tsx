62f133d27095fff285841d378fc15aec84dd9e63
luc <luc.leray@gmail.com>
2022-11-17 14:24:49 +0100

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import '../components/system2/style.css'
 import '../components/system2/photos.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
