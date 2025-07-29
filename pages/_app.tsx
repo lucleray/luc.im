22450b32182621d427028b248f1c29ac7fe2b7ac
Luc Leray <luc.leray@gmail.com>
2025-07-29 23:40:50 +0200

    Revert "Try with react"
    This reverts commit af8f1bbb294b6c66973c234b298c321da430fe96.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import '../components/system2/style.css'
 import '../components/system2/photos.css'
